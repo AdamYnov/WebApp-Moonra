64068eb7bdd6e7db900f9ff8c11f43894c18c547
AdamYnov <adam.ouerfelli@ynov.com>
2022-03-09 10:29:08 +0100

    m

--- src/views/MonCompte/MonCompte.vue
+++ src/views/MonCompte/MonCompte.vue
@@ -1,6 +1,27 @@
 <template>
 
+  <h1 class="text-3xl font-bold underline">
+    Mon Compte
+  </h1>
+
+
+
 </template>
 
 <script>
+//import Navbar from '@/components/Navbar.vue'
+
+//export default {
+//  name: 'MonCompte',
+//  components: {
+//    Navbar
+//  }
+//}
 </script>
+
+
+<style scoped>
+.yo {
+  color: grey;
+}
+</style>
